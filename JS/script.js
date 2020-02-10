1eb15e13a6955abcdc6395073e2287ccbfab49e0
Lance Smith <lance.a.smith23@gmail.com>
2020-02-10 18:23:40 +0000

    Added to projects page

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".carousel");
-  var instances = M.Carousel.init(elems, { duration: 100 });
+  var instances = M.Carousel.init(elems, { fullWidth: true });
 });
 
-var instance = M.Carousel.init({
-  fullWidth: true
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, { accordion: true });
 });
+
+var instance = M.Collapsible.getInstance(elem);
